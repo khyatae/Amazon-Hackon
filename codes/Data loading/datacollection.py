9101e4ded1c15010200561f9d0ef9906e78bb7df
khyatae <khyathi2003kotipalli@gmail.com>
2024-06-15 18:32:00 +0530

    Addes sentiment analysis part

--- codes/Data loading/datacollection.py
+++
@@ -1,8 +0,0 @@
-import datasets 
-datasets.logging.set_verbosity_error()
-
-from datasets import load_dataset
-
-dataset = load_dataset("McAuley-Lab/Amazon-Reviews-2023", "raw_review_Amazon_Fashion", trust_remote_code=True)
-
-print(dataset["full"][0])
